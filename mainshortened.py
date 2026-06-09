e129bcd4832deebbfdf0be212999addf6f42e054
Sourodip1804 <sourodipmahato762@gmail.com>
2026-06-09 10:52:30 +0530

    Add files via upload

---
+++ mainshortened.py
@@ -0,0 +1,40 @@
+import random
+'''
+1 for snake
+-1 for water 
+0 for gun
+
+'''
+computer=random.choice([-1,0,1])
+youstr=input("enter the choice: ")
+youdict={"s":1,"w":-1,"g":0}
+reversedict={1:"snake",-1:"water",0:"gun"}
+you=youdict[youstr]
+print(f"You choice{reversedict[you]}\ncomputer chose{reversedict[computer]}")
+if (computer==you):
+    print("its a draw")
+else:
+    if ((computer- you)==-1 or (computer-you)==2):
+        print("you lose")
+    else:
+        print("you  win")
+
+'''
+                                     #Logic
+    if (computer==-1 and you==1):  (computer - you)=-2
+        print("You win")
+    elif (computer==-1 and you==0): (computer - you)=-1
+        print("You lose")
+    elif (computer==1 and you==-1): (computer - you)=2
+        print("you lose!")
+    elif (computer==1 and you==0):  (computer - you)=1
+        print("You win!")
+    elif (computer==0 and you==-1): (computer - you)=1
+        print("You win!")
+    elif (computer==0 and you==1): (computer - you)=-1
+        print("You lose!")
+    else:
+        print("something went wrong")
+
+
+'''
